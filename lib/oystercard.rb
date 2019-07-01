1fd5ddafa47f95bf3dc1e8ef591e6803db894f19
TBrockster <tobybrock93@gmail.com>
2019-07-01 16:18:38 +0100

    challenge 8 complete

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -7,16 +7,44 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
     raise "error: top-up exceeds maximum (#{DEFAULT_MAXIMUM})" if max?(amount)
+
     @balance += amount
   end
 
-private
+  def deduct(amount)
+    raise 'error: insufficient funds' if enough?(amount)
 
-def max?(amount)
-@balance + amount > DEFAULT_MAXIMUM
-end
+    @balance -= amount
+  end
+
+  def touch_in
+    raise 'error: Already in journey' if in_journey?
+
+    @in_journey = true
+  end
+
+  def touch_out
+    raise 'error: Not in journey' unless in_journey?
+
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  private
+
+  def max?(amount)
+    @balance + amount > DEFAULT_MAXIMUM
+  end
+
+  def enough?(amount)
+    amount > @balance
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,14 +10,50 @@ describe Oystercard do
     end
   end
   describe '#top_up' do
-    it { is_expected.to respond_to(:top_up).with(1).argument}
+    it { is_expected.to respond_to(:top_up).with(1).argument }
     it 'adds 10 to our balance' do
-    expect { subject.top_up(10) }.to change { subject.balance }.from(0).to(10)
+      expect { subject.top_up(10) }.to change { subject.balance }.from(0).to(10)
+    end
+    it 'raises exception when top-up exceeds default maximum' do
+      maximum_balance = Oystercard::DEFAULT_MAXIMUM
+      subject.top_up(maximum_balance)
+      expect { subject.top_up(2) }.to raise_error "error: top-up exceeds maximum (#{Oystercard::DEFAULT_MAXIMUM})"
+    end
   end
-  it 'raises exception when top-up exceeds default maximum' do
-    maximum_balance = Oystercard::DEFAULT_MAXIMUM
-    subject.top_up(maximum_balance)
-    expect { subject.top_up(2) }.to raise_error "error: top-up exceeds maximum (#{Oystercard::DEFAULT_MAXIMUM})"
+  describe '#deduct' do
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+    it 'deducts 10 from our balance' do
+      subject.top_up(20)
+      expect { subject.deduct(10) }.to change { subject.balance }.by(-10)
+    end
+    it 'raises exception when deduct exceeds balance' do
+      subject.top_up(20)
+      expect { subject.deduct(30) }.to raise_error 'error: insufficient funds'
+    end
   end
-end
+  describe '#in_journey' do
+    it 'new instances start outside of a journey' do
+      expect(subject.in_journey?).to eq false
+    end
+  end
+  describe '#touch_in' do
+    it 'causes in_journey? to return true' do
+      subject.touch_in
+      expect(subject.in_journey?).to eq true
+    end
+    it 'raises error if already in journey' do
+      subject.touch_in
+      expect { subject.touch_in }.to raise_error 'error: Already in journey'
+    end
+  end
+  describe '#touch_out' do
+    it 'causes in_journey? to return false' do
+      subject.touch_in
+      subject.touch_out
+      expect(subject.in_journey?).to eq false
+    end
+    it 'raises error if not in journey' do
+      expect { subject.touch_out }.to raise_error 'error: Not in journey'
+    end
+  end
 end
