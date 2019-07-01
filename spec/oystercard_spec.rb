c51339ccd33ed7460fe6738f4e905231782fab2c
TBrockster <tobybrock93@gmail.com>
2019-07-01 14:50:39 +0100

    challenge 4 completed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 require 'Oystercard'
 
 describe Oystercard do
-
+  describe '#balance' do
+    it { is_expected.to respond_to(:balance) }
+    it 'new instances initialize with a balance of 0' do
+      expect(subject.balance).to eq 0
+    end
+  end
 end
