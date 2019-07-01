635e399056911ab9e5f589378cc6c86c46aae089
Student <student@Makerss-MacBook-Pro.local>
2019-07-01 15:11:49 +0100

    step 6

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,4 +9,10 @@ describe Oystercard do
       expect(subject.balance).to eq 0
     end
   end
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument}
+    it 'adds 10 to our balance' do
+    expect { subject.top_up(10) }.to change { subject.balance }.from(0).to(10)
+  end 
+end
 end
