7753a1d4b270a7b0ab68001459670353447e3c10
Student <student@Makerss-MacBook-Pro.local>
2019-07-01 15:39:09 +0100

    start step 7

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,12 +3,20 @@
 # this class simulates oystercards
 class Oystercard
   attr_reader :balance
+  DEFAULT_MAXIMUM = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
-    @balance += amount 
+    raise "error: top-up exceeds maximum (#{DEFAULT_MAXIMUM})" if max?(amount)
+    @balance += amount
   end
+
+private
+
+def max?(amount)
+@balance + amount > DEFAULT_MAXIMUM
+end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,6 +13,11 @@ describe Oystercard do
     it { is_expected.to respond_to(:top_up).with(1).argument}
     it 'adds 10 to our balance' do
     expect { subject.top_up(10) }.to change { subject.balance }.from(0).to(10)
-  end 
+  end
+  it 'raises exception when top-up exceeds default maximum' do
+    maximum_balance = Oystercard::DEFAULT_MAXIMUM
+    subject.top_up(maximum_balance)
+    expect { subject.top_up(2) }.to raise_error "error: top-up exceeds maximum (#{Oystercard::DEFAULT_MAXIMUM})"
+  end
 end
 end
